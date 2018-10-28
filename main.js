251c9b8ca5b4cc8cdc8271693a988a2e4d443bd4
Paul Martella <paulmartella01@gmail.com>
2018-10-27 22:42:59 -0500

    dew it

---
+++ main.js
@@ -0,0 +1,31 @@
+//public api requests
+
+    /*
+    GET wss://api.gemini.com/v1/marketdata/BTCUSD
+    Connection: Upgrade
+    Upgrade: websocket
+    Sec-WebSocket-Key: uRovscZjNol/umbTt5uKmw==
+    Sec-WebSocket-Version: 13
+    */
+
+// Private api requests
+
+    /*
+    GET wss://api.gemini.com/v1/order/events
+    Connection: Upgrade
+    Upgrade: websocket
+    Sec-WebSocket-Key: uRovscZjNol/umbTt5uKmw==
+    Sec-WebSocket-Version: 13
+    X-GEMINI-APIKEY: qOfnZJDZTTBsxdM3bVRP
+    X-GEMINI-PAYLOAD: eyJyZXF1ZXN0IjoiL3YxL29yZGVyL2V2ZW50cyIsIm5vbmNlIjoxNDc3OTYzMjQwNzQxMDgzMzA3fQ==
+    X-GEMINI-SIGNATURE: 88cd6f391d8f920a76a2060d613b519a8e8b4b3fb5bff089ea826d49ac73888bd479c0c2e2062ba60ba7afbe273132e3
+    */
+    
+// get all avaliable symbols for trading 
+    // GET https://api.gemini.com/v1/symbols
+
+    function tradingAlert() {
+        
+    }
+
+    
