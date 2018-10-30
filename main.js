3298c6192faa865a7aaffcad7f47622db2dc6d08
Paul Martella <paulmartella01@gmail.com>
2018-10-29 22:29:10 -0500

    all

--- main.js
+++ main.js
@@ -61,9 +61,32 @@ $(document).ready(function() {
             crossDomain: true,
             success: function (res) {
                 console.log(res.ytdChangePercent);
-                if (sessionStorage.time == "ytdChangePercent" )
-                priceChange = res.ytdChangePercent;
-                console.log("price change is: " + priceChange);
+                if (sessionStorage.time == "ytdChangePercent") {
+                    priceChange = res.ytdChangePercent;
+                    console.log("price change is: " + priceChange);
+                } else if (sessionStorage.time == "year5ChangePercent") {
+                    priceChange = res.year5ChangePercent;
+                    console.log("price change is: " + priceChange);
+                } else if (sessionStorage.time == "year2ChangePercent") {
+                    priceChange = res.year2ChangePercent;
+                    console.log("price change is: " + priceChange);
+                } else if (sessionStorage.time == "year1ChangePercent") {
+                    priceChange = res.year1ChangePercent;
+                    console.log("price change is: " + priceChange);
+                } else if (sessionStorage.time == "month6ChangePercent") {
+                    priceChange = res.month6ChangePercent;
+                    console.log("price change is: " + priceChange);
+                } else if (sessionStorage.time == "month3ChangePercent") {
+                    priceChange = res.month3ChangePercent;
+                    console.log("price change is: " + priceChange);
+                } else if (sessionStorage.time == "month1ChangePercent") {
+                    priceChange = res.month1ChangePercent;
+                    console.log("price change is: " + priceChange);
+                } else {
+                    priceChange = res.day5PercentChange;
+                    console.log("price cahnge is: " + priceChange);
+                }
+                
                 priceCalc = Math.round(priceChange) * 100;
                 document.getElementById('output').value= "Change over time: %" + priceCalc;
             },
